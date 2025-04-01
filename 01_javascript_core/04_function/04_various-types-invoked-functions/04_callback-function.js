67e5b4c60db1cf5280968c78c58ebbe8e4371059
Bluesky3125 <charlie4822@gmail.com>
2025-04-01 14:01:12 +0900

    2025/04/01 14:00 오후 전

---
+++ 01_javascript_core/04_function/04_various-types-invoked-functions/04_callback-function.js
@@ -0,0 +1,23 @@
+// 콜백함수 1: 다른 함수에 호출되는 함수
+function increase(value) {
+    return value + 1;
+};
+
+// 콜백함수 2
+function decrease(value) {
+    return value - 1;
+};
+
+// 고차함수: 다른 함수를 호출하는 함수
+function apply(func, value) {
+    return func(value);
+}
+
+console.log(apply(increase, 5));    // increase 함수가 콜백
+console.log(apply(decrease, 5));    // decrease 함수가 콜백
+
+console.log([3, 2, 1, 5, 4].sort());
+console.log([3, 2, 1, 5, 4].sort(function(left, right) {return right - left;}));
+console.log([3, 2, 1, 5, 4].sort((left, right) => right - left));
+
+// 비동기 처리(이벤트, 타이머, ajax, fetch, ...)
